6d500c9a1bcff27dfb93840775e16e977d815e8e
cocoHkx <3385208004@qq.com>
2021-04-26 15:29:24 +0800

    测试navmesh

---
+++ project/Assets/Scripts/PlayerData/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    //public GameObject Dest;
+    public Camera cam;
+    private NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        //agent.SetDestination(Dest.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit Hit;
+            if(Physics.Raycast(ray,out Hit))
+            {
+                agent.SetDestination(Hit.point);
+            }
+        }
+    }
+}
