690d0bb09e82ada9ace4bfbd4d0d9b14428c8fb7
DESKTOP-I5R18UN\dell <616706258@qq.com>
2021-05-19 11:06:12 +0800

    NPC

---
+++ project/Assets/PrefabPeople/scripts/PeopleAnim.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeopleAnim : MonoBehaviour
+{
+
+    private Animator animator;
+    public string pose;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        animator = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool(pose, true);
+    }
+}
